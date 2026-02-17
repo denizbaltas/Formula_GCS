7b8b819ac3e7755fa659ec36c6858d83f4ab2438
chocomilk05 <iremrmak.nler@gmail.com>
2026-02-17 22:15:23 +0300

    gui, main ve plotting yapıldı

---
+++ LTS/gui.py
@@ -0,0 +1,32 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1023, 744)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.track_map_widget = QtWidgets.QWidget(parent=self.centralwidget)
+        self.track_map_widget.setObjectName("track_map_widget")
+        self.verticalLayout.addWidget(self.track_map_widget)
+        self.curvature_widget = QtWidgets.QWidget(parent=self.centralwidget)
+        self.curvature_widget.setObjectName("curvature_widget")
+        self.verticalLayout.addWidget(self.curvature_widget)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1023, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
---
+++ LTS/plotting.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_track(ax, x_raw, y_raw, x_s, y_s, s, kappa, R, apex_idx):
+    """
+    Track Map (Top View) çizimi:
+    - x_raw, y_raw -> ham pist (scatter)
+    - x_s, y_s     -> spline edilmiþ pist (line)
+    - apex_idx     -> viraj tepe noktalarý (marker)
+    """
+
+
+    #Ham pist (scatter)
+    plt.scatter(x_raw, y_raw, color='blue', s=40, label='Raw Track')
+
+    #Spline edilmiþ pist (line)
+    ax.plot(x_s, y_s, color='HotPink', linewidth=2, label='Spline Track')
+
+    #Apex noktalarý
+    ax.scatter(
+        x_s[apex_idx],
+        y_s[apex_idx],
+        color='red',
+        s=120,
+        marker='*',
+        label='Apex Points'
+    )
+
+    ax.set_title("Track Map (Top View)")
+    ax.set_xlabel("X [m]")
+    ax.set_ylabel("Y [m]")
+    ax.axis('equal')     # Pist ölçeði bozulmasýn
+    ax.grid(True)
+    ax.legend()
+
+
+
+
+def plot_curvature_heatmap(ax, x_s, y_s, kappa, apex_idx):
+
+    sc = ax.scatter(x_s, y_s, c = kappa, cmap = "viridis", s = 30)
+    #draws points at x_s[i],y_s[i]
+    #color for each point comes from kappa[i]
+    #viridis colormap, size of marker is s
+
+    ax.figure.colorbar(sc, label = "Curvature Kappa") # adds the color scale legend on the side
+
+    ax.scatter(x_s[apex_idx], y_s[apex_idx], color = "HotPink", s = 80, marker = "*", label = "Apex")
+
+    ax.axis("equal")
+    ax.grid(True)
+    ax.set_xlabel("X [m]")
+    ax.set_ylabel("Y [m]")
+    ax.set_title("Curvature Map")
+    ax.legend()
